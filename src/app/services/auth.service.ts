fb1b503558793b2365cbc345652840ea0436af62
Mohammad Taha Yaseen <mohammady@dewsolutions.in>
2025-10-29 17:55:05 +0530

    env

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
-import { environment } from '@environments/environment';
+import { environment } from 'src/environments/environment';
 
 interface AuthResponse {
   token: string;
